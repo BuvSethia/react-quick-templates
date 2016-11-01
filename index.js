5be3652ce59c1daa3b0f402d3b7a64b0976742fe
Sumbhav Sethia <sumbhav.sethia@gmail.com>
2016-11-01 15:11:21 -0700

    Created skeleton for index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+/**
+ * @file
+ * Module's exported functions all reside here
+ *
+ */
+
+'use strict';
+var write = require('write');
+
+/**
+ * Creates a ReactJS skeleton component based on the user's specifications
+ * @param {String} version - Should the template be ES5 or ES6
+ * @param {String} type - Is this a presentation or container component
+ * @param {String} path - Path of the file to be created
+ */
+function createTemplateFile(version, type, path) {
+
+}
+
+module.exports = createTemplateFile;
