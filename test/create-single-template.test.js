81ffe668a3885dd230011865689c023adcdddbec
Sumbhav Sethia <sumbhav.sethia@gmail.com>
2016-11-09 16:35:06 -0700

    Added testing and removing index.js (forgot to add that to the last commit)

---
+++ test/create-single-template.test.js
@@ -0,0 +1,48 @@
+var fs = require('fs');
+var test = require('ava');
+// var rimraf = require('rimraf');
+var rqt = require('../rqt');
+
+// All created files will go in this directory
+var created = './created';
+
+test('Testing is good to go', t => {
+	t.pass();
+});
+
+test.cb('ES5 container named Hello in a new directory - POSIX filepath', t => {
+	// The control to which we will compare the created file
+	var compareFilePath = './compare/es5/Hello.js';
+	var createFilePath = created + '/es5c/Hello.js';
+
+	rqt.createComponent('es5', 'c', createFilePath, function (created) {
+		// If the file was created the function should return true
+		t.true(created);
+
+		fs.exists(createFilePath, function (exists) {
+			t.true(exists);
+		});
+
+		fs.readFile(compareFilePath, 'utf-8', function (errorCompare, compareData) {
+			// If this error is hit there is something wrong with the test
+			if (errorCompare) {
+				t.fail('TEST ERROR: Error reading control file ' + compareFilePath);
+			}
+
+			fs.readFile(createFilePath, 'utf-8', function (errorCreate, createData) {
+				if (errorCreate) {
+					t.fail('Error reading created file ' + createFilePath);
+				}
+
+				t.is(createData, compareData);
+				t.end();
+			});
+		});
+	});
+});
+
+test.after.always('Clean up created directory', () => {
+	/* rimraf(created, function (error) {
+		t.fail('Could not clean created directory with error: ' + error);
+	}); */
+});
---
+++ test/created/es5c/Hello.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var React = require('react');
+var PropTypes = React.PropTypes;
+
+var Hello = React.createClass({
+	getInitialState: function () {
+		return {
+
+		};
+	},
+
+	render: function () {
+		return (
+			<div>
+
+			</div>
+		);
+	},
+
+	propTypes: {
+
+	}
+});
+
+module.exports = Hello;
